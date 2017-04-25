267dc57e350026361cc8d32fd532ae2c91ebb97e
Tom Taylor <hello@tomtaylor.name>
2017-04-25 10:59:05 +0100

    Update entry point

--- frontend/assetManagement/index.js
+++ frontend/assetManagement/index.js
@@ -1,23 +1,19 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-  var Handlebars = require('handlebars');
+  var Origin = require('core/origin');
+  var AssetModel = require('./models/assetModel');
+  var AssetCollection = require('./collections/assetCollection');
+  var AssetManagementView = require('./views/assetManagementView');
+  var AssetManagementSidebarView = require('./views/assetManagementSidebarView');
+  var AssetManagementNewAssetView = require('./views/assetManagementNewAssetView');
+  var AssetManagementNewAssetSidebarView = require('./views/assetManagementNewAssetSidebarView');
+  var CourseCollection = require('core/collections/courseCollection');
+  var TagsCollection = require('core/collections/tagsCollection');
 
-  var Origin = require('coreJS/app/origin');
-  var AssetCollection = require('coreJS/assetManagement/collections/assetCollection');
-  var AssetManagementNewAssetView = require('coreJS/assetManagement/views/assetManagementNewAssetView');
-  var AssetManagementNewAssetSidebarView = require('coreJS/assetManagement/views/assetManagementNewAssetSidebarView');
-  var AssetManagementSidebarView = require('coreJS/assetManagement/views/assetManagementSidebarView');
-  var AssetManagementView = require('coreJS/assetManagement/views/assetManagementView');
-  var AssetModel = require('coreJS/assetManagement/models/assetModel');
-  var CourseModel = require('editorCourse/models/editorCourseModel');
-  var CourseAssetModel = require('editorCourse/models/editorCourseAssetModel');
-  var ProjectCollection = require('coreJS/project/collections/projectCollection');
-  var TagsCollection = require('coreJS/tags/collections/tagsCollection');
-
-  Origin.on('app:dataReady login:changed', function() {
+  Origin.on('origin:dataReady login:changed', function() {
     Origin.globalMenu.addItem({
       "location": "global",
-      "text": "Asset Management",
+      "text": Origin.l10n.t('app.assetmanagement'),
       "icon": "fa-file-image-o",
       "callbackEvent": "assetManagement:open",
       "sortOrder": 2
@@ -25,11 +21,13 @@ define(function(require) {
   });
 
   Origin.on('globalMenu:assetManagement:open', function() {
-    Origin.router.navigate('#/assetManagement', { trigger: true });
+    Origin.router.navigateTo('assetManagement');
   });
 
   Origin.on('router:assetManagement', function(location, subLocation, action) {
-    Origin.assetManagement = { filterData: {} };
+    Origin.assetManagement = {
+      filterData: {}
+    };
 
     if(!location) {
       loadCollectionView();
@@ -43,18 +41,18 @@ define(function(require) {
   });
 
   function loadCollectionView() {
-    new TagsCollection().fetch({
+    (new TagsCollection()).fetch({
       success: function(tags) {
         // Sidebar also needs access to collection, so create now. Fetch is done
         // in collectionView (thanks to server-side filtering)
         Origin.trigger('location:title:hide');
         Origin.sidebar.addView(new AssetManagementSidebarView({ collection: tags }).$el);
-        Origin.router.createView(AssetManagementView, { collection: new AssetCollection() });
+        Origin.contentPane.setView(AssetManagementView, { collection: new AssetCollection() });
         Origin.trigger('assetManagement:loaded');
       },
       error: handleError
     });
-  };
+  }
 
   function loadAssetView(id) {
     var isNew = id === undefined;
@@ -65,11 +63,11 @@ define(function(require) {
     var data = isNew ? {} : { _id: id };
     var model = new AssetModel(data);
     // needed for filtering
-    new ProjectCollection().fetch({
-      success: function(projects) {
-        model.set('projects', filterProjects(projects));
+    (new CourseCollection()).fetch({
+      success: function(courses) {
+        model.set('projects', filterProjects(courses));
         if(isNew) loadView();
-        else model.fetch({ success:loadView });
+        else model.fetch({ success: loadView });
       }
     });
 
@@ -78,9 +76,9 @@ define(function(require) {
         "backButtonText": "Back to assets",
         "backButtonRoute": "/#/assetManagement"
       });
-      Origin.router.createView(AssetManagementNewAssetView, { model: model });
+      Origin.contentPane.setView(AssetManagementNewAssetView, { model: model });
     };
-  };
+  }
 
   // filters out projects that aren't 'mine' OR shared
   // @return just the  _ids and titles
@@ -95,34 +93,13 @@ define(function(require) {
         title: project.get('title')
       };
     });
-  };
+  }
 
   function handleError() {
+    // TODO localise this
     Origin.Notify.alert({
       type: 'error',
       text: 'An error occured fetching data - try refreshing your page'
     });
-  };
-
-  Handlebars.registerHelper('cacheBuster', function(context) {
-    var currentUser = Origin.sessionModel.get('user');
-    if(!currentUser) return '';
-
-    var lastSession = new Date(currentUser.get('lastAccess'));
-    var lastUpdated = new Date(context.updatedAt);
-    if(lastSession < lastUpdated) return '?' + new Date().getTime()
-  });
-
-  Handlebars.registerHelper('getAssetIcon', function(context) {
-    switch(context.assetType) {
-      case 'image':
-        return 'fa-image-o';
-      case 'video':
-        return 'fa-video-o';
-      case 'audio':
-        return 'fa-audio-o';
-      default:
-        return 'fa-file-o';
-    }
-  });
+  }
 });
